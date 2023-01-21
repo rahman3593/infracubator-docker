13381840527e9f3b18c3124908aa81ad994dacae
Abdul Rahman <72451246+rahman3593@users.noreply.github.com>
2023-01-21 17:32:46 +0530

    Delete docker_assignment directory

--- docker_assignment/assignment3/system_capabilities.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "current date: $(date +"%d-%m-%y")"
-echo "OS Information: $(cat /etc/os-release)"
-echo "CPU Information: $(cat /proc/cpuinfo)"
-echo "Memory Information:: $(cat /proc/meminfo)"
